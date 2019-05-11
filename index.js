8dd0ea487907b3328f8749e72b77c296f1aeb361
Dmeowmixer <ransonikuo@gmail.com>
2019-05-11 09:49:50 -1000

    final

--- index.js
+++ index.js
@@ -1,27 +1,49 @@
 const functions = require('firebase-functions');
-const webhookURL = "https://hooks.slack.com/services/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";
 const admin = require('firebase-admin');
 const request = require('request-promise');
 
 admin.initializeApp();
 
 exports.lincoln = functions.database.ref('lincoln').onWrite((change, context) => {
-  const afterData = change.after.val();
+  let beforeData = change.before.val();
+  let afterData = change.after.val();
 
-  const anonDonations = 
+  let beforeDataLength = Object.keys(beforeData).length;
+  let afterDataLength = Object.keys(afterData).length;
+  let newDonations = afterDataLength - beforeDataLength;
+  console.log(beforeDataLength, afterDataLength);
+  let anonDonorsCount = 0;
 
+  let anonDonors = afterData.forEach(x => {
+    if(x.donor_name === ""){
+      anonDonorsCount++;
+    }
+  })
 
-  console.log(afterData)
+  let percentage = anonDonorsCount / Object.keys(afterData).length;
+  let percentageValue = percentage.toFixed(2) * 100
+
   var options = {
     url: "https://hooks.slack.com/services/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",
     method: "POST",
     body: {
-      "text": "Total Number of Uploads: " + Object.keys(afterData).length,
-      "text": "Hello world?"
+      text: "A file was successfully uploaded",
+      "attachments": [
+        {
+          name: "Number of new donations",
+          text: "Number of new donations: " + newDonations
+        },{
+          name: "Total number of donations",
+          text: "Total number of donations: " + Object.keys(afterData).length
+        },{
+          name: "Percentage of anon donations",
+          text: "Percentage of anonymous donations: " + Math.floor(percentageValue) + "%"
+        }
+      ]
     },
     json: true // Automatically stringifies the body to JSON
   };
-  return request(options)
+  return request(options);
 });
 
 // webhook todo
